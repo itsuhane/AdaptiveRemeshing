28cd2092c0b50821477864a72431969efaead73f
Jinyu Li <itsuhane@gmail.com>
2017-06-05 10:54:50 +0800

    Normalize indentation & minor changes.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-
 #include <iostream>
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include "Remesher.h"
 
 int main(int argc, char *argv[]) {
-	typedef OpenMesh::TriMesh_ArrayKernelT<RemesherTraits> Mesh;
+    typedef OpenMesh::TriMesh_ArrayKernelT<RemesherTraits> Mesh;
     Mesh mesh;
     OpenMesh::IO::read_mesh(mesh, argv[1]);
-	Remesher<Mesh> remesher;
-	remesher.run(mesh, 0.001f);
+    Remesher<Mesh> remesher;
+    remesher.run(mesh, 0.001f);
     OpenMesh::IO::write_mesh(mesh, argv[2]);
     return 0;
 }
